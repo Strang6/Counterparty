fd9773e6f383ab1c8be0f62837c36340c1c7715f
Strang6 <dariagorod@gmail.com>
2017-12-12 22:09:18 +0300

    import

--- app/src/main/java/com/strang6/counterparty/database/AddressCoordinatesDAO.java
+++ app/src/main/java/com/strang6/counterparty/database/AddressCoordinatesDAO.java
@@ -1,14 +1,12 @@
 package com.strang6.counterparty.database;
 
 import android.arch.persistence.room.Dao;
-import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.TypeConverters;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.strang6.counterparty.AddressCoordinates;
-import com.strang6.counterparty.RecentCounterparty;
 import com.strang6.counterparty.database.converters.LatLngConverter;
 
 import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
