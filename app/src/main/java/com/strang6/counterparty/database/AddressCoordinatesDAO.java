8ddef5d4b4d73f180896ad36060169377ba012d7
Strang6 <dariagorod@gmail.com>
2017-12-12 21:44:28 +0300

    onSwiped Delete fix

--- app/src/main/java/com/strang6/counterparty/database/AddressCoordinatesDAO.java
+++ app/src/main/java/com/strang6/counterparty/database/AddressCoordinatesDAO.java
@@ -1,12 +1,14 @@
 package com.strang6.counterparty.database;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.TypeConverters;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.strang6.counterparty.AddressCoordinates;
+import com.strang6.counterparty.RecentCounterparty;
 import com.strang6.counterparty.database.converters.LatLngConverter;
 
 import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
@@ -23,4 +25,7 @@ public interface AddressCoordinatesDAO {
 
     @Insert(onConflict = REPLACE)
     void  addCoordinates(AddressCoordinates addressCoordinates);
+
+    @Query("DELETE FROM AddressCoordinates WHERE counterpartyId = :id")
+    void deleteAddressById(String id);
 }
